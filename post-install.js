d3c42100fdc37eb3d5d80906b5887a235c4baa57
Sagar Chauhan <coolsagar005@gmail.com>
2023-02-23 12:14:50 +0530

    Fixed : all tested and fully stable now for our projects

--- post-install.js
+++ post-install.js
@@ -32,10 +32,10 @@ async function addCustomScripts(scripts) {
 
 // Define the scripts to add
 const scripts = {
-    'pause-smart-commit': 'mv .git/hooks/pre-commit-msg .git/hooks/pre-commit-msg.sample',
-    'restart-smart-commit': 'mv .git/hooks/pre-commit-msg.sample .git/hooks/pre-commit-msg',
-    'install-smart-commit': 'mv node_modules/git-smart-commit/src/hooks/prepare-commit-msg.sh .git/hooks/pre-commit-msg',
-    'uninstall-smart-commit': 'rm .git/hooks/pre-commit-msg',
+    'pause-smart-commit': 'mv .git/hooks/prepare-commit-msg .git/hooks/prepare-commit-msg.sample',
+    'restart-smart-commit': 'mv .git/hooks/prepare-commit-msg.sample .git/hooks/prepare-commit-msg',
+    'install-smart-commit': 'mv node_modules/git-smart-commit/src/hooks/prepare-commit-msg.sh .git/hooks/prepare-commit-msg',
+    'uninstall-smart-commit': 'rm .git/hooks/prepare-commit-msg',
 };
 
 // Add the scripts to package.json
